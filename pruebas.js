02c5e1079486dc8ec68b36a43709c8fb8d49eb17
AndreyBlanco <andygblanco@gmail.com>
2024-10-03 17:35:49 -0600

    router

--- pruebas.js
+++ pruebas.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var app = express();
-
-app.get('/', (req, res) => {
-    res.send("Hola");
-})
+const router = express.Router();
+ 
+router.get('/home', (req, res) => {
+  res.send('Hello World, This is home router');
+});
+ 
+router.get('/profile', (req, res) => {
+  res.send('Hello World, This is profile router');
+});
+ 
+router.get('/login', (req, res) => {
+  res.send('Hello World, This is login router');
+});
+ 
+router.get('/logout', (req, res) => {
+  res.send('Hello World, This is logout router');
+});
+ 
+app.use('/', router);
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
