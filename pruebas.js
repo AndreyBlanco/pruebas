617bff7b24a3625ce89f646c83ed921b2b0f985b
AndreyBlanco <andygblanco@gmail.com>
2024-10-02 18:59:01 -0600

    first send

--- pruebas.js
+++ pruebas.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.get('/', (req, res) => {
+    res.send("Hola");
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
 });
